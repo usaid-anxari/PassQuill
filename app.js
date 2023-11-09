e7ed242b4031c3220b7443eee951e76913c73573
usaid-anxari <usaidax@gmail.com>
2023-11-09 22:23:50 +0500

    Project is Pending

---
+++ app.js
@@ -0,0 +1,25 @@
+const passwordBox = document.getElementById("password");
+const passowrdLenght = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = '0123456789'
+const symbles = ':;"<>^&*()_+=%$#@!`~{[}]|?';
+let allChars = upperCase + lowerCase + number + symbles; 
+
+
+let createPassword = () => {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbles[Math.floor(Math.random() * symbles.length)];
+  while (passowrdLenght > password.length) {
+       password += allChars[Math.floor(Math.random() * allChars.length)]
+  }
+  passwordBox.value = password;
+};
+
+let copyPassword =()=>{
+    passwordBox.select()
+    document.execCommand('copy')
+}
